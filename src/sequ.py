1c0e41755102c3b874225c9e66fe4972b2c49609
Kristina Frye <kfrye@psu.edu>
2013-11-02 12:27:54 -0700

    Got first and increment arguments to work as optional. Changed for loop to while loop to work with floats since range only works with ints

--- src/sequ.py
+++ src/sequ.py
@@ -2,22 +2,21 @@
 
 # Copyright 2013 Kristina Frye
 # CS 300
-# sequ command, Compliance Level 0
-# October 4, 2013
+# sequ command, Compliance Level 1
+# November 2, 2013 
 #
-# This command takes exactly 2 arguments that must be integers
-# and prints out all integers between these numbers (inclusive)
-# Success returns a 0
-# Failure returns a 1
 
 # argparse allows us to easily parse the input arguments
 import argparse
 
 parser = argparse.ArgumentParser(description='The sequ command prints a '
    'sequence of numbers between the "first" and the "last" argument '
-   'in increments of 1.') 
-parser.add_argument('first', help='The starting integer', type=int)
-parser.add_argument('last', help='The ending integer', type=int)
+   'in increments of 1.')
+parser.add_argument('first', help='The starting integer', type=float, nargs='?',
+   default=1)
+parser.add_argument('increment', help='The increment between numbers',
+   type=float, nargs='?', default=1)
+parser.add_argument('last', help='The ending integer', type=float)
 
 # We use a try/except in order to override the exit status code with 1
 # This code is from:
@@ -38,8 +37,10 @@ if(args.last - args.first > 100000000):
    exit(1)
 
 # Print the sequential numbers
-for i in range(args.first, args.last + 1):
+i = args.first
+while(i <= args.last):
    print i
-
+   i = i + args.increment
+ 
 # Success!
 exit(0)
