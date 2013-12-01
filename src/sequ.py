a8bf5a51c8874d60835046c3c186f4d1a7f5d080
Kristina Frye <kfrye@psu.edu>
2013-12-01 14:44:39 -0800

    Added -F/--format_word parsing

--- src/sequ.py
+++ src/sequ.py
@@ -12,6 +12,7 @@ import sys
 import argparse
 from decimal import Decimal
 import roman
+import pdb
 
 def get_version():
    return 'sequ version 1.20, written by Kristina Frye'
@@ -98,6 +99,16 @@ def isRoman(num):
    else:
       return False
 
+def isUpperRoman(s):
+   if isRoman(s.upper()) and isUpperAlpha(s):
+      return True
+   return False
+
+def isLowerRoman(s):
+   if isRoman(s.upper()) and isLowerAlpha(s):
+      return True
+   return False 
+
 # How to determine string is a number:
 # http://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-is-a-number-in-python 
 def isFloat(input_string):
@@ -113,20 +124,25 @@ def isInteger(input_string):
          return True
    return False
 
+def isUpperAlpha(input_string):
+   if(str(input_string).isupper()):
+      return True
+   return False
+
+def isLowerAlpha(input_string):
+   if(str(input_string).islower()):
+      return True
+   return False
+
 # Takes a string and returns 'R' for upper case roman, 'r' for lower case
 # roman, 'A' for upper case alpha, 'a' for lower case alpha, 'i' for integer, 
-# 'f' for float, and '0' for none of the above
+# 'f' for float, and '0' for none of the above, Since alpha always has
+# preference over roman, roman is never set as the default type
 def getType(s):
    if(s.isupper()):
-      if(isRoman(s)):
-         return 'R'
-      else:
-         return 'A'
+      return 'A'
    elif(s.islower()):
-      if(isRoman(s.upper())):
-         return 'r'
-      else:
-         return 'a'
+      return 'a'
    elif(isInteger(s)):
       return 'i'
    elif(isFloat(s)):
@@ -140,12 +156,9 @@ def fromCharString(s):
    num = ord('a') - 1
    
    for i in range(length - 1, -1, -1):
-      if(i == length - 1):
-         num += ord(s[i]) - ord('a') + 1
-      else:
-         letter = ord(s[i]) - ord('a') + 1
-         letter = letter * 26**(length - i - 1)
-         num += letter
+      letter = ord(s[i]) - ord('a') + 1
+      letter = letter * 26**(length - i - 1)
+      num += letter
 
    return num
 
@@ -160,8 +173,7 @@ def getCharString(num, input_type):
    return char_string
 
 # Converts a string to a number based upon the input type of the string
-def convertToNum(s):
-   input_type = getType(s)
+def convertToNum(input_type, s):
    if(input_type == 'R'):
       num = roman.fromRoman(s)
    elif(input_type == 'r'):
@@ -177,8 +189,13 @@ def convertToNum(s):
 # Class used to store the type and numeric value of a number 
 class SequValue:
    def __init__(self, s):
+      self.input_string = s
       self.value_type = getType(s)
-      self.num = convertToNum(s)
+      self.num = convertToNum(self.value_type, s)
+
+   def setType(self, input_type):
+      self.value_type = input_type
+      self.num = convertToNum(self.value_type, self.input_string)
 
 # Returns a roman numeral string based upon a number and the type 
 # (lower case vs upper case roman)
@@ -203,42 +220,201 @@ def getLetter(num, repeat):
    return_string += chr((num % 26) + ord('a'))
    return return_string
 
-
-
 # Returns a SequValue object with defaults appropriate for a beginning
 # value for the type. For instance, type 'A' should begin with a capital 'A'
 def setDefaults(value_type):
-   if(value_type == 'r'): 
-      default = SequValue('i') 
-   elif(value_type == 'R'): 
-      default = SequValue('I') 
-   elif(value_type == 'i' or value_type == 'f'):
-      default = SequValue('1')
+   if(value_type == 'r' or value_type == 'R' or value_type == 'i' or 
+      value_type == 'f'): 
+      default = SequValue('1') 
    elif(value_type == 'a'): 
       default = SequValue('a')
    elif(value_type == 'A'):
       default = SequValue('A')
    return default
-       
+
+# This function is used when format_word is specified. It checks that
+# all the arguments will work with the format_word
+def parseFormatWord(args, first, last, increment):
+   isParsed = True
+   format_word = args.format_word
+   value_type = last.value_type
+
+   print("Format word: ", format_word)
+   # If roman, check that the arguments can parse as roman
+   if(format_word == 'roman' or format_word == 'ROMAN'):
+      if((isUpperRoman(args.last) == True and format_word == 'ROMAN') or
+         (last.value_type == 'i' and format_word == 'ROMAN')):
+         value_type = 'R'
+      
+      if((isLowerRoman(args.last) == True and format_word == 'roman') or
+         (last.value_type == 'i' and format_word == 'roman')):
+         value_type = 'r'
+      
+      else:
+         isParsed = False
+
+      # The first and increment arguments can also be integer 
+      if(args.first != None and first.value_type != 'i'):
+         if((value_type == 'r' and isLowerRoman(args.first) == False) or
+            (value_type == 'R' and isUpperRoman(args.first) == False)):
+            isParsed = False
+
+      if(args.increment != None and increment.value_type != 'i'):
+         if((value_type == 'r' and isLowerRoman(args.increment) == False) or
+            (value_type == 'R' and isUpperRoman(args.increment) == False)):
+            isParsed = False
+
+   # If alpha, check that the arguments can parse as alpha
+   if(format_word == 'alpha' or format_word == 'ALPHA'):
+      pdb.set_trace()
+      if(isUpperAlpha(args.last) and format_word == 'ALPHA'):
+         value_type = 'A'
+      elif(isLowerAlpha(args.last) and format_word == 'alpha'):
+         value_type = 'a'
+      else:
+         isParsed = False
+
+      if(args.first != None):
+         if((value_type == 'a' and isLowerAlpha(args.first) == False) or 
+            (value_type == 'A' and isUpperAlpha(args.first) == False)):
+            isParsed = False
+      
+      # The increment argument for alpha must be integer
+      if(args.increment != None):
+         if(increment.value_type != 'i'):
+            isParsed = False
+
+   # If arabic, check that all the other arguments are also integer
+   if(format_word == 'arabic'):
+      if(last.value_type != 'i'):
+         isParsed = False
+      else:
+         value_type = 'i'
+
+      if(args.first != None and first.value_type != 'i'):
+         isParsed = False
+
+      if(args.increment != None and increment.value_type != 'i'):
+         isParsed = False
+
+   # If floating, check that allt he other arguments can be parsed as floats
+   if(format_word == 'floating'):
+      if(isFloat(args.last) == True):
+         value_type = 'f'
+      else:
+         isParsed = False
+
+      if(args.first != None and isFloat(args.first) == False):
+         isParsed = False
+
+      if(args.increment != None and isFloat(args.increment) == False):
+         isParsed = False
+
+   # Return whether arguments passed checks and the input_type for the
+   # last argument
+   return isParsed, value_type
+
+
 # This function runs a few checks of the input for validity
 def check_inputs(args):
    last = SequValue(args.last)
-   print("Last type: ", last.value_type)
-   print("Last: ", last.num)
+#   print("Last type: ", last.value_type)
+#   print("Last: ", last.num)
    if(last.value_type == '0'):
       print('The "last" value is not valid')
       exit(1)
 
    if(args.first != None):
+#      pdb.set_trace()
       first = SequValue(args.first)
-      print("First: ", first.num)
+#      print("First: ", first.num)
    else:
       first = setDefaults(last.value_type)
 
    if(args.increment != None):
+#     pdb.set_trace()
       increment = SequValue(args.increment)
    else:
       increment = SequValue("1") 
+
+#   print("Increment: ", increment.num)
+ 
+   if(args.format_word != None):
+      # Check the arguments and the format word agree
+      parse_format_word = parseFormatWord(args, first, last, increment)
+      #print("Parse format word: ", parse_format_word[0], parse_format_word[1])
+   #  pdb.set_trace()
+      if(parse_format_word[0] == False):
+         print("The arguments do not agree with the specified format word")
+         exit(1)
+      else:
+         # If we changed the format type to roman, make sure the 
+         # first and increment format types are also set to roman
+         if(last.value_type == 'i' and 
+            (parse_format_word[1] == 'r' or parse_format_word[1] == 'R')):
+            last.value_type = 'r' 
+         else:   
+            last.setType(parse_format_word[1])
+
+         if(last.value_type == 'r'):
+            if(args.first != None and first.value_type != 'i'):
+               first.setType('r')
+            if(args.increment != None and increment.value_type != 'i'):
+               increment.setType('r')
+            if(args.first == None):
+               first = setDefaults('r')
+
+         elif(last.value_type == 'R'):
+            if(args.first != None and first.value_type != 'i'):
+               first.setType('R')
+            if(args.increment != None and increment.value_type != 'i'):
+               increment.setType('R')
+            if(args.first == None):
+               first = setDefaults('R') 
+         
+   if(last.value_type == 'a' or last.value_type == 'A'):
+      if(args.first != None):
+         if(first.value_type != last.value_type):
+            print("The 'first' and 'last' arguments must be of the same type")
+            exit(1)
+
+      if(args.increment != None):
+         if(increment.value_type != 'i'):
+            print("Alpha arguments must use an arabic (integer) increment")
+            exit(1)
+ 
+   elif(last.value_type == 'i'):
+      if(args.first != None):
+         if(first.value_type != 'i' and first.value_type != 'f'):
+            print("The 'first' and 'last' argument must be of the same type")
+            exit(1)
+
+      if(args.increment != None):
+         if(increment.value_type != 'i' and 
+            increment.value_type != first.value_type):
+            print("The 'first' and 'increment' arguments must be of the same "+
+               "type")
+            exit(1)
+
+   elif(last.value_type == 'f'):
+      if(args.first != None):
+         if(first.value_type != 'i' and first.value_type != last.value_type):
+            print("The 'first' and 'last' arguments must be of the same type")
+            exit(1)
+
+      if(args.increment != None):
+         if(increment.value_type != last.type and increment_value_type != 'i'):
+            print("The 'increment' and 'last' arguments must be of the same "+
+               "type")
+            exit(1) 
+
+   elif(last.value_type == 'r' or last.value_type == 'R'):
+      if(last.num > 4999 or first.num > 4999 or increment.num > 4999):
+         print("Roman numbers may only be used with numbers up to 4999.")
+         exit(1)
+      if(args.string_format != None):
+         print("Roman numbers cannot be used with the special format option.")
+         exit(1)
 
    # Exit with success if nothing to print
    if(first.num > last.num):
@@ -260,6 +436,7 @@ def check_inputs(args):
          print("You need to specify a one character padding.")
          exit(1)
 
+#   print("Got to end of parse function")
    return first, last, increment
 
 # This function prints the number sequence
@@ -373,6 +550,10 @@ parser.add_argument('first',
 parser.add_argument('increment', 
    help='The increment between numbers (default=1)', nargs='?')
 parser.add_argument('last', help='The ending integer')
+parser.add_argument('-F', '--format_word', 
+   help='Print with arabic (for integers), floating, alpha (for characters), '+ 
+   'ALPHA (for capital characters), roman (for roman numerals), ROMAN (for '+
+   'capital roman numerals).') 
 group.add_argument('-f', '--format', dest='string_format', 
    help='Print with special formatting: %%x, %%X, %%g, %%G, %%f, %%F', 
    nargs='?')
